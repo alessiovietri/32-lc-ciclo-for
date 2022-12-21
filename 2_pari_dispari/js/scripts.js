e78245ab1689ebab9ffbb35aa96b3bf757eb9087
Alessio Vietri <info@alessiovietri.it>
2022-12-21 12:32:23 +0100

    Completato LC 2

---
+++ 2_pari_dispari/js/scripts.js
@@ -0,0 +1,32 @@
+// console.log('PRIMA DEL CICLO FOR');
+
+// for (let i = 0; i < 10; i++) {
+
+//     console.log('DURANTE IL CICLO FOR', i);
+
+//     document.getElementById('mio-div').innerHTML += '<p>' + (i + 1) + '° P</p>';
+
+// } 
+
+// console.log('DOPO IL CICLO FOR');
+
+// let risultatoDivisione = (10 / 3);
+// let risultatoModulo = (10 % 3);
+// console.log('risultatoDivisione 10 / 3: ' + risultatoDivisione);
+// console.log('risultatoModulo 10 % 3: ' + risultatoModulo);
+
+
+
+
+
+
+
+
+let numeroQuasiCasuale = 98764;
+// Verificare se numeroQuasiCasuale è pari. Se è pari, alertare "Pari", altrimenti alertare "Dispari"
+if (numeroQuasiCasuale % 2 == 0) {
+    alert("Pari");
+}
+else {
+    alert("Dispari");
+}
