f8d82bc764be278475acb49c1f7a518ecfe1ec4a
Alessio Vietri <info@alessiovietri.it>
2022-12-21 12:56:36 +0100

    Completato LC 4

---
+++ 4_tabelline/js/scripts.js
@@ -0,0 +1,13 @@
+for (let tabellina = 1; tabellina <= 10; tabellina++) {                         // CICLO ESTERNO
+
+    console.log('--------------------------------------------------');
+
+    for (let moltiplicatore = 1; moltiplicatore <= 10; moltiplicatore++) {      // CICLO INTERNO
+
+        console.log(tabellina + ' * ' + moltiplicatore + ' = ', tabellina * moltiplicatore);
+
+    }
+
+    console.log('--------------------------------------------------');
+    
+}
